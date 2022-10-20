fdde31adf617e968da92c07941ea2d5315eeef16
Ashwin solanki <ashwin@ninjatechnolabs.com>
2022-10-20 14:03:22 +0530

    camera image upload\

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, FlatList, StyleSheet, TouchableOpacity, Alert, SafeAreaView } from "react-native";
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Alert, SafeAreaView, PermissionsAndroid } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import CatCard from "../Components/CatCard";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import ListEmpty from "../Components/ListEmpty";
 // import { toggleToaster } from "../redux/actions";
 import { toggleToaster } from "../redux/counter";
+import * as ImagePicker from "expo-image-picker";
+import * as Permission from "expo-permissions";
+import { launchCamera } from "react-native-image-picker"
 
 const Home = (props) => {
     const dispatch = useDispatch()
@@ -13,6 +16,11 @@ const Home = (props) => {
     const { setShowForm, setSelectedId, setFormData } = props;
 
     const { cats, showToaster, toasterBackgroundColor, toasterMessage } = useSelector(s => s.catsList);
+
+    let options = {
+        // saveToPhotos: true,
+        mediaType: "photo"
+    }
 
     // UseEffect to handle toaster hide after every 3 sec of display
     useEffect(() => {
@@ -35,6 +43,75 @@ const Home = (props) => {
         setFormData({ name: "", breed: "", description: "" });
     }
 
+    const pickFromCamera = async () => {
+        // const { granted } = await Permissions.askAsync(Permissions.CAMERA)
+        if (true) {
+            let data = await ImagePicker.launchCameraAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                allowsEditing: true,
+                aspect: [1, 1],
+                quality: 0.5
+            })
+            console.log(data)
+            if (!data.cancelled) {
+                let newFile = { uri: data.uri, type: `test/${data.uri.split(".")[1]}`, name: `text.${data.uri.split(".")[1]}` }
+
+                handleUpload(newFile)
+            }
+        }
+        else {
+            Alert.alert("you need to give permission to work")
+        }
+    }
+
+    const openCamera = async () => {
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA,
+            {
+                title: "Cool Photo App Camera Permission",
+                message:
+                    "Cool Photo App needs access to your camera " +
+                    "so you can take awesome pictures.",
+                buttonNeutral: "Ask Me Later",
+                buttonNegative: "Cancel",
+                buttonPositive: "OK"
+            }
+        )
+
+        console.log("granted", granted)
+        console.log("permission", PermissionsAndroid.RESULTS.GRANTED)
+
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            const result = await launchCamera(options)
+
+            console.log("results", result)
+
+            const uri = result.assets[0].uri
+            let newFile = { uri: uri, type: `test/jpg`, name: uri.split("/").pop() }
+
+            console.log("newFile", newFile)
+
+            handleUpload(newFile)
+            // console.log("inside if", result)
+        }
+    }
+
+    const handleUpload = (image) => {
+        const data = new FormData();
+        data.append("file", image)
+        data.append("upload_preset", "employeeApp");
+        data.append("cloud_name", "dez1ir6xd")
+
+        console.log("data", data)
+
+        // fetch("https://cloudinary://291132195717678:GsRyS8Dh7JQQlqY1Gg0J142vN2I@dez1ir6xd/image/upload", {
+        // fetch("https://api.cloudinary.com/v1_1/dez1ir6xd/image/upload", {
+        fetch("https://api.cloudinary.com/v1_1/dez1ir6xd/image/upload", {
+            method: "post",
+            body: data
+        }).then(res => res.json()).then(data => console.log("data", data)).catch(err => console.log("err", err))
+    }
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             {cats.length ? <Text style={styles.heading}>Have a look at your cats </Text> : null}
@@ -46,7 +123,9 @@ const Home = (props) => {
                 contentContainerStyle={{ flexGrow: 1 }}
                 ListEmptyComponent={() => <ListEmpty />}
             />
-            <TouchableOpacity onPress={handleAdd} style={styles.addIcon}>
+            {/* <TouchableOpacity onPress={handleAdd} style={styles.addIcon}> */}
+            {/* <TouchableOpacity onPress={pickFromCamera} style={styles.addIcon}> */}
+            <TouchableOpacity onPress={openCamera} style={styles.addIcon}>
                 <Ionicons name="add" color="#000" size={30} />
             </TouchableOpacity>
             {showToaster && <View style={[styles.toaster, { backgroundColor: toasterBackgroundColor }]}>
